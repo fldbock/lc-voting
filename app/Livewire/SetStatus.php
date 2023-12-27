35d71878ef385b586f66e6e4c97859b8fb8bc211
fldbock <flor.debock@gmail.com>
2023-12-27 11:03:06 +0100

    Set Status Notify All Voters

--- app/Livewire/SetStatus.php
+++ app/Livewire/SetStatus.php
@@ -2,6 +2,9 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\IdeaStatusUpdatedMailable;
+
 use Illuminate\Http\Response;
 use Livewire\Component;
 
@@ -11,12 +14,13 @@ use App\Models\User;
 class SetStatus extends Component
 {
     public $idea;
-
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea){
         $this->idea = $idea;
         $this->status = $this->idea->status_id;
+        $this->notifyAllVoters = false;
     }
 
     public function setStatus(){
@@ -27,10 +31,25 @@ class SetStatus extends Component
         $this->idea->status_id = $this->status;
         $this->idea->save();
 
+        if($this->notifyAllVoters){
+            $this->notifyAllVoters();
+        }
+
         $this->dispatch('statusWasUpdated');
     }
     public function render()
     {
         return view('livewire.set-status');
+    }
+
+    public function notifyAllVoters(){
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function($voters) {
+                foreach($voters as $user){
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
+                }
+            });
     }
 }
